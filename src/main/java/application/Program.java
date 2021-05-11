e497e45f5c7680b1a2881f09abb3114bd68ea1b5
ThiRO85 <thgr85@gmail.com>
2021-05-11 20:47:26 -0300

    Persistindo objetos em banco de dados

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import domain.Person;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Person p1 = new Person(null, "Carlos da Silva", "carlos@testmail.com");
+		Person p2 = new Person(null, "Joaquim Torres", "joaquim@testmail.com");
+		Person p3 = new Person(null, "Ana Maria", "anamaria@testmail.com");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto!");
+	}
+}
